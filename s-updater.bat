bd49a1fb1452931b431c73a0511ebafdb72a13cf
Siiriiks <jonnymillington@gmail.com>
2020-07-18 16:34:14 +0100

    Fix sleep for some users

--- s-updater.bat
+++ s-updater.bat
@@ -2,6 +2,7 @@
 title Dolphin Game Store - Updater
 cd /D "%~dp0"
 set wget="..\Installer\wget.exe"
+set sleep="..\Installer\sleep.exe"
 cls
 
 echo ----------------------------------------------------------------------------------------------------
@@ -9,12 +10,12 @@ echo Dolphin Game Store - Updater
 echo ----------------------------------------------------------------------------------------------------
 echo.
 echo Removing previous files...
-sleep 2
+%sleep% 2
 del store.bat
 echo Downloading Update...
 %wget% https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/store.bat -q --show-progress --no-check-certificate
 echo Finished Downloading!
 echo Launching Dolphin Game Store...
-timeout 2
+%sleep% 2
 start store.bat
 exit
