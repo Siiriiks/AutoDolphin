b0e7049e5afa27f4692f36b09fa9d3a06eaed651
Siiriiks <jonnymillington@gmail.com>
2020-07-18 17:05:32 +0100

    Fix Updater
    + Delete the right files
    + Install new command-line tools if they are available

--- updater.bat
+++ updater.bat
@@ -1,16 +1,23 @@
 @echo off
 title Dolphin Installer - Updater
 cd /D "%~dp0"
+set wget="%~dp0wget.exe"
+set sleep="%~dp0sleep.exe"
 cls
 
 echo Removing previous files...
-sleep 2
-del store.bat
+%sleep% 1
+del Installer.bat
+del Setup.bat
+del tools.zip
 echo Downloading Update...
-wget https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/Installer.bat -q --show-progress --no-check-certificate
-wget https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/Setup.bat -q --show-progress --no-check-certificate
+%wget% https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/Installer.bat -q --show-progress --no-check-certificate
+%wget% https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/Setup.bat -q --show-progress --no-check-certificate
+%wget% https://raw.githubusercontent.com/Siiriiks/AutoDolphin/master/tools.zip -q --show-progress --no-check-certificate
+7za.exe x tools.zip -y
+del tools.zip
 echo Finished Downloading!
 echo Launching Dolphin Installer...
-timeout 2
+%sleep% 2
 start Installer.bat
 exit
